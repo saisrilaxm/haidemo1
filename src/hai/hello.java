8774ac20bc79204c9f1acbd55d2ecec8283e9c41
lakshmineha <srilaxmi550@gmail.com>
2023-07-08 16:11:12 +0530

    commited

---
+++ src/hai/hello.java
@@ -0,0 +1,17 @@
+package hai;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO  Auto-generated method stub
+		//System.out.println("hai");
+		int a=10;
+		int b=20;
+		//int c=a+b;
+		System.out.println("int c=a+b");
+		
+		
+	}
+
+}
+ 
